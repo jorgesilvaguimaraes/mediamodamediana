96d4ca7d61eeeedb91e61602e89df367fcbd2b24
Jorge da Silva Guimarães <jorgeguimaraes@cinbesa.com.br>
2020-03-27 09:58:03 -0300

    Meida, Moda e Mediana Codenation Java Online

---
+++ src/main/java/br/com/codenation/StatisticUtil.java
@@ -0,0 +1,47 @@
+package br.com.codenation;
+
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.Arrays.asList;
+
+public class StatisticUtil {
+
+	public static int average(int[] elements) {
+		return (int) Arrays.stream(elements).average().getAsDouble();
+	}
+
+	public static int mode(int[] elements) {
+
+		return IntStream.of(elements)
+				.boxed()
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+				.entrySet()
+				.stream()
+				.max(Comparator.comparingLong(Map.Entry::getValue))
+				.get().getKey();
+	}
+
+	public static int median(int[] elements) {
+
+		int[] lista = Arrays.stream(elements).sorted().toArray();
+		int size = lista.length;
+
+		if( size % 2 == 1 ){
+			return lista[Math.floorDiv(size,2)];
+		}else{
+			return (lista[Math.floorDiv(size,2)] + lista[(Math.floorDiv(size,2) - 1)]) / 2;
+		}
+	}
+
+	public static void main(String[] args){
+
+		int[] elements = new int[]{2,2,2,2,3,3,3,3,6,7,7,7,7,7};
+		System.out.println(mode(elements));
+	}
+}
